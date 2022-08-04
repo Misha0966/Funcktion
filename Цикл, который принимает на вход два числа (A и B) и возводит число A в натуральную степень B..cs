b00d78db13373ead032d94ae9f4ca3d17d79ea07
Misha0966 <108358743+Misha0966@users.noreply.github.com>
2022-08-04 23:54:36 +0300

    Задача 25
    Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
    Ответы:
    
    3, 5 -> 243 (3⁵)
    
    2, 4 -> 16

---
+++ Цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B..cs
@@ -0,0 +1,23 @@
+﻿//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+using System;
+using static System.Console;
+
+int A = AskNumber("A");
+int B = AskNumber("B");
+WriteLine($"{A},{B} => {Exponentiation(A,B)}");
+
+int AskNumber(string name) {
+    Write($"Введите число {name}: ");
+    return int.Parse(ReadLine());
+}
+
+int Exponentiation(int A, int B) {
+    int result = 1;
+    for(int i = 0; i < B; i++) {
+        result = result*A;
+    }
+    return result;
+}
